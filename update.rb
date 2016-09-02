4841a47ea19affc99487b770427365786d0c69a6
Kepler Sticka-Jones <kepler+git@stickajones.org>
2016-09-02 17:27:07 -0600

    update v1.0.0

--- update.rb
+++ update.rb
@@ -1,9 +1,8 @@
 class Update < Formula
   desc "Utility to update all of your system's package in one go."
   homepage "https://github.com/keplersj/update"
-  url "https://github.com/keplersj/update/archive/v0.1.0.tar.gz"
-  # version "0.1.0" # Audit say that the version number is taken from the URL.
-  sha256 "166c50f7aa03d6c08b5893f587d68e18a0a47535cc719835cbaf186f826b3753"
+  url "https://github.com/keplersj/update/archive/v1.0.0.tar.gz"
+  sha256 "cdf9f34ca4906ef0f67372f522ade0b192453f80a3db6871b2dd06c77e06eaf5"
   head "https://github.com/keplersj/update.git"
 
   depends_on "bdw-gc"
